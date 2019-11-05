43b006881dec24d2b673692cd74cc109c1579761
cenuno <24255819+cenuno@users.noreply.github.com>
2019-11-05 08:51:44 -0800

    Break out log into two separate files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # install necessary packages
 # note: storing everything printed to the Terminal to the log/log.txt file
-sh src/requirements/install.sh | tee -a log/log.txt
+sh src/requirements/install.sh | tee -a log/01_install_log.txt
 
 # download necessary data and place it in a PostgreSQL database
 # note: storing everything printed to the Terminal to the log/log.txt file
-sh src/data/get_data.sh | tee -a log/log.txt
+sh src/data/get_data.sh | tee -a log/02_get_data_log.txt
