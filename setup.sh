a6ce237da0d6f5c246695bdc4ec62350b9b5b0e9
cenuno <24255819+cenuno@users.noreply.github.com>
2019-11-04 18:15:13 -0800

    Store terminal output to log/log.txt file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 # install necessary packages
-sh src/requirements/install.sh
+# note: storing everything printed to the Terminal to the log/log.txt file
+sh src/requirements/install.sh | tee -a log/log.txt
 
 # download necessary data and place it in a PostgreSQL database
-sh src/data/get_data.sh
+# note: storing everything printed to the Terminal to the log/log.txt file
+sh src/data/get_data.sh | tee -a log/log.txt
