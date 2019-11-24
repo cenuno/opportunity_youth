02a92dfed934cd2ad332d4ad167a3050650a8054
cenuno <24255819+cenuno@users.noreply.github.com>
2019-11-23 16:10:48 -0800

    Log std error during installation

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # install necessary packages
-# note: storing everything printed to the Terminal to the log/log.txt file
-sh src/requirements/install.sh | tee -a log/01_install_log.txt
+# note: storing everything printed to the Terminal to log/
+sh src/requirements/install.sh 2>&1 | tee -a log/01_install_log.txt
 
 # download necessary data and place it in a PostgreSQL database
-# note: storing everything printed to the Terminal to the log/log.txt file
-sh src/data/get_data.sh | tee -a log/02_get_data_log.txt
+# note: storing everything printed to the Terminal to log/
+sh src/data/get_data.sh 2>&1 | tee -a log/02_get_data_log.txt
